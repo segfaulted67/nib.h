16c26f1dd449b77bc0919ffc86b60a7e4430f55a
larry <segfaulted67@gmail.com>
2025-12-02 16:06:27 +0530

    fix: fixed segfault issue

--- nib.c
+++ nib.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#define NIB_H_IMPLEMENTATION
+#define NIB_IMPLEMENTATION
 #include "nib.h"
 
 int main(void)
 {
   nib_Cmd cmd = { 0 };
-  nib_cmd_append(&cmd, "cc", "-o", "main", "main.c");
-  for(size_t i = 0; i < cmd.count; i++)
-  {
-    nib_Info("%s", cmd.items[i]);
-  }
+  nib_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "--std=c11", "-o", "main", "main.c");
   nib_cmd_run(cmd);
   return 0;
 }
--- nib.h
+++ nib.h
@@ -1,9 +1,9 @@
 #ifndef NIB_H
 #define NIB_H
 
-// TODO: Fix segfaulted issue
-#ifdef NIB_H_IMPLEMENTATION
-#if defined(__cplusplus)
+
+#ifdef NIB_IMPLEMENTATION
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -24,6 +24,7 @@ extern "C" {
 #endif
 // ------------------------------------------------------------------------------------------------------------------
 // Constants
+#define NIB_INVALID_PROC (-1)
 #define DA_INITIAL_CAP 256
 // ------------------------------------------------------------------------------------------------------------------
 // Colors
@@ -42,18 +43,37 @@ extern "C" {
 
 // ------------------------------------------------------------------------------------------------------------------
 // Logging
-#define nib_Log(stream, color, level, fmt, ...)  fprintf(stream, color "[" level "] " fmt "\n", ##__VA_ARGS__)
-#define nib_Error(fmt, ...) nib_Log(stderr, NIB_C_RED, "Error", NIB_C_RESET "%s:%d (%s()): " fmt, ##__VA_ARGS__)
-#define nib_Info(fmt, ...)  nib_Log(stdout, NIB_C_BLUE, "Info", NIB_C_RESET fmt, ##__VA_ARGS__)
-#define NIB_ASSERT(x)   assert(x)
+
+#ifdef ANSI_ENABLE
+#define nib_Log(stream, color, level, fmt, ...) \
+    fprintf(stream, color "[%s] " NIB_C_RESET fmt"\n", \
+            level, ##__VA_ARGS__)
+
+#define nib_Error(fmt, ...) \
+    nib_Log(stderr, NIB_C_RED, "Error", \
+            "%s:%d (%s()): "fmt, \
+            __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+
+#define nib_Info(fmt, ...) \
+    nib_Log(stdout, NIB_C_BLUE, "Info", fmt, ##__VA_ARGS__)
+#else
+#define nib_Log(stream, level, fmt, ...)  \
+  fprintf(stream, "[%s] " fmt "\n", level, ##__VA_ARGS__)
+#define nib_Error(fmt, ...) \
+  nib_Log(stderr, "Error", "%s:%d (%s())" fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define nib_Info(fmt, ...)  \
+  nib_Log(stdout, "Info", fmt, ##__VA_ARGS__)
+#endif
+#define nib_Assert(x)   assert(x)
 
 // Foreach loop
-#define nib_foreach(type, it, da) for (type *it = (da)->items; it < (da)->items + (da)->capacity; ++it)
+#define nib_foreach(type, it, da)     \
+  for (type *it = (da)->items; it < (da)->items + (da)->count; ++it)
 
 #ifdef __cplusplus
-#define NIB_DECTYPE_CAST(T) (decltype(T))
+#define NIB_DECLTYPE_CAST(T) (decltype(T))
 #else
-#define NIB_DECTYPE_CAST(T)
+#define NIB_DECLTYPE_CAST(T)
 #endif
 
 // typecasting useful C functions
@@ -75,12 +95,12 @@ typedef struct{
 } nib_StringBuilder;
 
 #ifdef _WIN32
-typedef HANDEL Pid;
+typedef HANDLE Pid;
 #else
 typedef int Pid;
 #endif
 // ------------------------------------------------------------------------------------------------------------------
-// Functions
+//
 #define nib_da_reserve(da, expected_capacity)                                     \
   do {                                                                            \
     if(expected_capacity > (da)->capacity)                                        \
@@ -91,19 +111,14 @@ typedef int Pid;
       while(expected_capacity > (da)->capacity) {                                 \
         (da)->capacity *= 2;                                                      \
       }                                                                           \
-      (da)->items = NIB_DECTYPE_CAST((da)->items)NIB_REALLOC((da)->items, (da)->capacity * sizeof((da)->items));      \
-      NIB_ASSERT((da)->items != NULL && "Bro dont have enough RAM in BIG 25 lol");  \
+      (da)->items = NIB_DECLTYPE_CAST((da)->items)NIB_REALLOC((da)->items, (da)->capacity * sizeof(*(da)->items));      \
+      nib_Assert((da)->items != NULL && "Bro dont have enough RAM in BIG 25 lol");  \
     }                                                                             \
   } while (0)
 
 #define nib_da_append(da, item)                                                           \
   do {                                                                                    \
-    if((da)->count >= (da)->capacity)                                                     \
-    {                                                                                     \
-      (da)->capacity = (da)->capacity == 0 ? DA_INITIAL_CAP : (da)->capacity * 2;         \
-      (da)->items = realloc((da)->items, (da)->capacity * sizeof(*(da)->items));          \
-      assert((da)->items != NULL && "Buy more RAM LOL!!");                                \
-    }                                                                                     \
+    nib_da_reserve((da), (da)->count + 1);                                                \
     (da)->items[(da)->count++] = (item);                                                  \
   } while (0)
 
@@ -114,7 +129,10 @@ typedef int Pid;
     (da)->count += (new_items_count);                                           \
   } while (0)
 
-#define nib_da_free(da) NIB_FREE((da).items)
+#define nib_da_free(da)   \
+  NIB_FREE((da)->items)
+
+// ------------------------------------------------------------------------------------------------------------------
 
 void nib_cmd_append_null(nib_Cmd *cmd, ...)
 {
@@ -142,21 +160,25 @@ Pid nib_cmd_run(nib_Cmd cmd)
   nib_da_append_many(&cmd_null, cmd.items, cmd.count);
   nib_cmd_append(&cmd_null, NULL);
 
+  if(cpid < 0)
+  {
+    nib_Error("Could not fork child process: %s", strerror(errno));
+    return NIB_INVALID_PROC;
+  }
+
   if(cpid == 0)
   {
-    if(execvp(cmd.items[0], (char * const*)cmd_null.items) < 0) {
+    if(execvp(cmd_null.items[0], (char * const*)cmd_null.items) < 0) {
       nib_Error("Could not exec child process for %s: %s", cmd.items[0], strerror(errno));
+      nib_da_free(&cmd_null);
       exit(1);
     }
   }
-  else
-  {
-    nib_Error("Could not exec child process for %s: %s", cmd.items[0], strerror(errno));
-  }
+  nib_da_free(&cmd_null);
   return cpid;
 }
+// ------------------------------------------------------------------------------------------------------------------
 
-#endif
 
 
 
@@ -164,8 +186,9 @@ Pid nib_cmd_run(nib_Cmd cmd)
 #define CMD nib_Cmd
 #define NIB_CMD nib_Cmd
 // ------------------------------------------------------------------------------------------------------------------
-#if defined(__cplusplus)
+#ifdef __cplusplus
 }
+#endif
 #endif
 
 #endif // NIB_H
